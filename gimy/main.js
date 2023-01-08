8838084431fa4d1f1adaecb69be291ebdf2641d3
turbo <2016devx@gmail.com>
2023-01-09 04:04:02 +0800

    update script

--- gimy/main.js
+++ gimy/main.js
@@ -1,6 +1,6 @@
 alert('inject..')
-let Oldone = '</body>'
-let Newone = '<link rel=\"stylesheet\" href=\"https://raw.githubusercontent.com/turboceo/adblock/main/gimy/main.css\" type=\"text/css\"></body>';
+let Oldone = '<div class="pc_rb_float" id="pc_rb_float">'
+let Newone = '';
 let body = $response.body
 .replace(Oldone, Newone);
 $done({body});
