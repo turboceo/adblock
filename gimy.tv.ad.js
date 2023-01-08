de8032f6561d3a6826f4096833f24ea527c29abb
turbo <2016devx@gmail.com>
2023-01-09 03:45:50 +0800

    update

--- gimy.tv.ad.js
+++ gimy.tv.ad.js
@@ -1,5 +1,5 @@
 alert('inject..')
-let Oldone = '</head>'
+let Oldone = '<head>'
 let Newone = '<head><link rel=\"stylesheet\" href=\"https://raw.githubusercontent.com/turboceo/adblock/main/gimy.tv.ad.css\" type=\"text/css\">';
 let body = $response.body
 .replace(Oldone, Newone);
