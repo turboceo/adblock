25d3e55d87487e1557479330ba42d0539527fb77
turbo <2016devx@gmail.com>
2023-01-09 04:43:10 +0800

    Update

--- gimy/main.js
+++ gimy/main.js
@@ -1,4 +1,4 @@
-$notification.post("这是主标题"，"这是副标题", "hello-cron!");
+$notification.post("这是主标题", "这是副标题", "hello-cron!");
 let Oldone = '3000'
 let Newone = '1000000000000';
 let body = $response.body
