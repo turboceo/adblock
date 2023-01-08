359c03e08edd3533c0be25f6701e31c8b514a203
turbo <2016devx@gmail.com>
2023-01-09 04:56:40 +0800

    update

--- gimy/main.js
+++ gimy/main.js
@@ -1,5 +1,6 @@
-alert('inject..')
-let Oldone = '</head>'
-let Newone = '<link rel=\"stylesheet\" href=\"https://raw.githubusercontent.com/turboceo/adblock/main/gimy/main.css\" type=\"text/css\"></head>';
+$notification.post("Debug", "Inject", "hello-cron!");
+let Oldone = "</head>";
+let Newone =
+  '<link rel="stylesheet" href="https://raw.githubusercontent.com/turboceo/adblock/main/gimy/main.css" type="text/css"></head>';
 let body = $response.body.replace(Oldone, Newone);
-$done({body});
+$done({ body });
