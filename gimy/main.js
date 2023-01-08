3deafeb44d5c5f3a8292339a273d8390ea802dec
turbo <2016devx@gmail.com>
2023-01-09 03:58:07 +0800

    Update

--- gimy/main.js
+++ gimy/main.js
@@ -1,6 +1,6 @@
 alert('inject..')
-let Oldone = '<head>'
-let Newone = '<head><link rel=\"stylesheet\" href=\"https://raw.githubusercontent.com/turboceo/adblock/main/gimy/main.css\" type=\"text/css\">';
+let Oldone = '</body>'
+let Newone = '<link rel=\"stylesheet\" href=\"https://raw.githubusercontent.com/turboceo/adblock/main/gimy/main.css\" type=\"text/css\"></body>';
 let body = $response.body
 .replace(Oldone, Newone);
 $done({body});
